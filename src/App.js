3c36d867c6afe955d8c2789ae0371235b0a89891
Ann <kiasanova@MacBook-Air-Ann.local>
2026-04-18 08:55:12 +0300

    состояния для спискка задач, задачи, даты, статус

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [filter, setFilter] = useState("All");
+  
   return <div></div>;
 }
 
