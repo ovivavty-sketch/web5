53e99bd4cdcd6e240dfcd44c48eb506ff8c44f44
Ann <kiasanova@MacBook-Air-Ann.local>
2026-04-18 09:31:49 +0300

    цвет

--- src/App.js
+++ src/App.js
@@ -1,63 +1,82 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
   const [name, setName] = useState("");
   const [date, setDate] = useState("");
-  const [filter, setFilter] = useState("All");
 
   const addTask = () => {
-  if (name === "" || date === "") return;
+    if (name === "" || date === "") return;
 
-  const newTask = {
-    id: Date.now(),
-    name: name,
-    date: date,
-    status: "To Do"
+    const newTask = {
+      id: Date.now(),
+      name: name,
+      date: date,
+      status: "To Do" // по умолчанию
+    };
+
+    setTasks([...tasks, newTask]);
+    setName("");
+    setDate("");
   };
 
-  setTasks([...tasks, newTask]);
-  setName("");
-  setDate("");
-};
-
-const changeStatus = (id)=>{
-  const updated = tasks.map((task)=>{
-    if (task.id === id) { 
-      if (task.status === "To Do") task.status = "In Progress"; 
-      else if (task.status === "In Progress") task.status = "Done";}
+  const changeStatus = (id) => {
+    const updated = tasks.map((task) => {
+      if (task.id === id) {
+        if (task.status === "To Do") task.status = "In Progress";
+        else if (task.status === "In Progress") task.status = "Done";
+      }
       return task;
-});
-setTasks (updated);};
+    });
 
-const filteredTasks = tasks.filter((task) => {
-  if (filter === "All") return true;
-  return task.status === filter;
-});
+    setTasks(updated);
+  };
 
-const today = new Date().toISOString().split("T")[0];
+  const today = new Date().toISOString().split("T")[0];
+const getColor = (task) => {
+  if (task.status === "To Do") return "red";
+  if (task.status === "In Progress") return "blue";
+  if (task.status === "Done") return "green";
+  return "black";
+};
+  return (
+    <div>
+      <h2>Задачи</h2>
+      <p>Сегодня: {today}</p>
 
-  return( <div>
-    <h2>Задачи</h2>
-    <p>Сегодня: {today}</p>
-    <input placeholder="НАзвание" value={name} onChange={(e)=>setName(e.target.value)}/>
-     <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-     <button onClick={addTask} >Добавить</button>
+      <input
+        placeholder="Название"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-    <ul>
-      {tasks.map((task)=>(
-        <li key ={task.id}> {task.name}| {task.date}|{task.status}</li>
-      )
-    )}
-    </ul>
-    <select onChange={(e) => setFilter(e.target.value)}>
-  <option>All</option>
-  <option>To Do</option>
-  <option>In Progress</option>
-  <option>Done</option>
-</select>
-     <button onClick={() => changeStatus(task.id)}> изменить статус </button>
-  </div>);
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+
+      <button onClick={addTask}>Добавить</button>
+
+      <ul>
+        {tasks.map((task) => {
+          const isLate = task.date < today && task.status !== "Done";
+
+          return (
+            <li
+              key={task.id}
+              style={{ color: getColor(task) }}
+            >
+              {task.name} | {task.date} | {task.status}
+              <button onClick={() => changeStatus(task.id)}>
+                изменить статус
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
