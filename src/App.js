827cdbe258af5e04b938ed95ca28652571e8ae0a
Ann <kiasanova@MacBook-Air-Ann.local>
2026-04-18 09:01:18 +0300

    функция добавл задачи

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ function App() {
   const [date, setDate] = useState("");
   const [filter, setFilter] = useState("All");
 
+  const addTask = () => {
+  if (name === "" || date === "") return;
+
+  const newTask = {
+    id: Date.now(),
+    name: name,
+    date: date,
+    status: "To Do"
+  };
+
+  setTasks([...tasks, newTask]);
+  setName("");
+  setDate("");
+};
+
   return( <div>
     <input placeholder="НАзвание" value={name} onChange={(e)=>setName(e.target.value)}/>
      <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
