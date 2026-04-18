085eeef8da8fb40616ba158fa234758f340b7d16
Ann <kiasanova@MacBook-Air-Ann.local>
2026-04-18 09:10:40 +0300

    изменение статуса задачи

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
   setDate("");
 };
 
+const changeStatus = (id)=>{
+  const updated = tasks.map((task)=>{
+    if (task.id === id) { 
+      if (task.status === "To Do") task.status = "In Progress"; 
+      else if (task.status === "In Progress") task.status = "Done";}
+      return task;
+});
+setTasks (updated);};
+
   return( <div>
     <input placeholder="НАзвание" value={name} onChange={(e)=>setName(e.target.value)}/>
      <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
